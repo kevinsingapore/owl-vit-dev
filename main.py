da1c364f7b9464f0c0c6aca3fe96c8ba23d0aa45
付强 <fuqiang_coding@coding.net>
2025-04-06 11:34:07 +0800

    save the image

--- main.py
+++ main.py
@@ -5,9 +5,10 @@
 # checkpoint = "google/owlvit-base-patch32"   
 # detector = pipeline(module=checkpoint, task="zero-shot-object-detection")
 
-import urllib3
+import urllib3,os
 from PIL import Image
 from io import BytesIO
+from datetime import datetime 
 
 url = "https://unsplash.com/photos/oj0zeY2Ltk4/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTR8fHBpY25pY3xlbnwwfHx8fDE2Nzc0OTE1NDk&force=true&w=640"
 
@@ -23,6 +24,13 @@ if response.status == 200:
     
     # 展示图片进行确认
     image.show()
+    imageFilename = 'google-out.png'
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    newImageFilename = 'google-out-' + timestamp + '.png'
+    if os.path.exists(imageFilename):
+        print(f'文件已经存在，已保存为:{newImageFilename}')
+        image.save(newImageFilename,'png')
+    image.save(imageFilename,'png')
     image.close()
 else:
     print(f"无法获取图像，状态码: {response.status}")
